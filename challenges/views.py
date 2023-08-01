70e0ac95556baddd92588bb6c9693d42ec8b0819
daniel <danielnobre09@gmail.com>
2023-08-01 19:05:53 -0300

    commit

--- challenges/views.py
+++ challenges/views.py
@@ -21,16 +21,11 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
-
-    response_data = f'<ul>{list_items}</ul>'
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months,
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -47,7 +42,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month,
+        })
     except:
         return HttpResponseNotFound("<h1>not found</h1>")
