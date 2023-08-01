a6a3328d00797160221c169f13db91c3464cd1f8
56dnsigma <danielnobre09@gmail.com>
2023-08-01 01:13:31 -0300

    commit

---
+++ challenges/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+
+monthly_challenges = {
+    "january": "january challenge",
+    "february": "february challenge",
+    "march": "march challenge",
+    "april": "april challenge",
+    "may": "may challenge",
+    "june": "june challenge",
+    "july": "july challenge",
+    "august": "august challenge",
+    "september": "september challenge",
+    "october": "october challenge",
+    "november": "november challenge",
+    "december": "december challenge",
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("invalid")
+    
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("not found")
