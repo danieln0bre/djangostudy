0771b7a547bb6926ff8923a0fe5cc474856ee492
daniel <danielnobre09@gmail.com>
2023-08-01 14:23:55 -0300

    commit

--- challenges/views.py
+++ challenges/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
-# test comment
+# test comment 2
 
 monthly_challenges = {
     "january": "january challenge",
